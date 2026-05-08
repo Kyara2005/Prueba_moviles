965baced6bd84e8f5f2848eb55e326761d549be4
Kyara <kyara.altamirano@epn.edu.ec>
2026-05-08 15:30:57 -0500

    primer commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://kjvhwpmnidljtjhsfiwb.supabase.co',
+  supabaseKey: 'sb_publishable_OiZJaifSATU0ztOqDpqzHw_v-p2579p'
 };
 
 /*
